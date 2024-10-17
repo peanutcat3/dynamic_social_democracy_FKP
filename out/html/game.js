9ad239912acd492e4b5f62de24d4351f675b05e0
originalperson0 <82953053+originalperson0@users.noreply.github.com>
2024-10-17 17:23:40 -0400

    addition

--- out/html/game.js
+++ out/html/game.js
@@ -201,6 +201,15 @@
         resultsDiv.style.display = 'none';
     }
 };
+  window.toggleGraph = function toggleGraph() {
+    const svgElement = document.getElementById('party_support_history');
+    if (svgElement.style.display === 'none' || svgElement.style.display === '') {
+        svgElement.style.display = 'block';
+    } else {
+        svgElement.style.display = 'none';
+    }
+}
+}
   /*
    * This function copied from the code for Infinite Space Battle Simulator
    *
