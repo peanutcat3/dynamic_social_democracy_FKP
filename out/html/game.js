1c9fd2cc4bf15e1ecda8aed4b829098b0adb9e16
originalperson0 <82953053+originalperson0@users.noreply.github.com>
2025-05-03 21:01:20 -0400

    content: papen-schleicher pt 4.7

--- out/html/game.js
+++ out/html/game.js
@@ -113,6 +113,17 @@
     window.dendryUI.saveSettings();
 };
 
+window.enableLightMode = function() {
+    window.dendryUI.dark_mode = false;
+    document.body.classList.remove('dark-mode');
+    window.dendryUI.saveSettings();
+};
+window.enableDarkMode = function() {
+    window.dendryUI.dark_mode = true;
+    document.body.classList.add('dark-mode');
+    window.dendryUI.saveSettings();
+};
+
   // populates the checkboxes in the options view
   window.populateOptions = function() {
     var disable_bg = window.dendryUI.disable_bg;
@@ -138,6 +149,11 @@
         $('#images_yes')[0].checked = true;
     } else {
         $('#images_no')[0].checked = true;
+    }
+    if (window.dendryUI.dark_mode) {
+        $('#dark_mode')[0].checked = true;
+    } else {
+        $('#light_mode')[0].checked = true;
     }
   };
 
@@ -301,6 +317,9 @@
 
   window.onload = function() {
     window.dendryUI.loadSettings({show_portraits: true});
+    if (window.dendryUI.dark_mode) {
+        document.body.classList.add('dark-mode');
+    }
     window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
     window.statusTab = "status";
     window.updateSidebar();
